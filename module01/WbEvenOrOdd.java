a285c741a1f57dda3427cc5d993aeeee365a20c0
Curt Grogan <curt.grogan@fdmgroup.com>
2022-04-25 14:21:25 -0400

    Loopy for loops

---
+++ module01/WbEvenOrOdd.java
@@ -0,0 +1,23 @@
+public class WbEvenOrOdd {
+  public static void main(String[] args) {
+    System.out.println();
+    // Task 1 – Make a for loop that counts from 0 to 19.
+    for (int i = 0; i <= 19; i++) {
+      if (i % 2 == 0) {
+        System.out.println(i + " - even");
+      } else {
+        System.out.println(i + " - odd");
+      }
+    }
+
+    /*
+     * Task 2
+     * 
+     * 1. If the number is even, print ' – even' beside the number.
+     * 2. If the number is odd, print ' – odd' beside the number.
+     * 
+     */
+    System.out.println();
+  }
+
+}
